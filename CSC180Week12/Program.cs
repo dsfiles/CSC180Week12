bd03fdd9a2de23efe19e46d0035d0d6475a9757a
Guangyan Li <GLi@stmartin.edu>
2022-11-17 08:52:43 -0800

    sadfsd

--- CSC180Week12/Program.cs
+++ CSC180Week12/Program.cs
@@ -1,11 +1,11 @@
-﻿public class Program
+﻿// version 4
+public class Program
 {
     static void Main(string[] args)
     {
-        Person person = new Person("Allan", 21);
         Teacher teacher = new Teacher("Tom", 55, "Computer Science");
-        teacher.DisplayNameAge();
+        teacher.DisplayTeacher();
         Student student = new Student("Sara", 19, 3.5);
-        student.DisplayNameAge();
+        student.DisplayStudent();
     }
 }
