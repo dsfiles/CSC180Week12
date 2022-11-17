34e29d33533b5c139bab2319ae8fff583739c921
Guangyan Li <GLi@stmartin.edu>
2022-11-17 08:19:00 -0800

    Add project files.

---
+++ CSC180Week12/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+	static void Main(string[] args)
+	{
+		Person person = new Person();
+		person.name = "Allan";
+		person.age = 21;
+
+		Teacher teacher = new Teacher();
+		teacher.name = "Tom";
+		teacher.age = 55;
+		teacher.subject = "Computer Science";
+		teacher.DisplayNameAge();
+
+		Student student = new Student();
+		student.name = "Sara";
+		student.age = 19;
+		student.gpa = 3.5;
+		student.DisplayNameAge();
+	}
+}
